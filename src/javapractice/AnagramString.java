bc69e2c302ecc90ee24df1bb54fab25a2dcb9e81
Nikita Prajapti <nikitaprajapati2112003@gmail.com>
2025-08-14 23:30:09 +0530

    check if two lowercase strings are anagrams using frequency array

---
+++ src/javapractice/AnagramString.java
@@ -0,0 +1,38 @@
+package javapractice;
+
+import java.util.Arrays;
+
+public class AnagramString {
+    public static boolean isAnagram(String str1, String str2) {
+        // Remove spaces and convert to lowercase for case-insensitive comparison
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths differ, they cannot be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to char arrays
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // Sort both arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Listen";
+        String s2 = "Silent";
+
+        if (isAnagram(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams.");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams.");
+        }
+    }
+}
